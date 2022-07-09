206694ee32ce4d3cad48162cf5a2bb6b941ffbd7
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-09 00:12:57 -0500

    feat: friends list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def my_portfolio
     @acciones_rastreadas = current_user.stocks
   end
+  
+  def my_friends
+    @mis_amigos = current_user.friends
+  end
 end
