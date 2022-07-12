255db74fa5bb18f0ea1b9a0fbe9ffe317aaf6062
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-12 17:32:13 -0500

    feat: Implement add friend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    friendship = Friendship.create(user_id: current_user.id , friend_id: friend.id)
+
+    if friendship.save
+      flash[:notice] = "Ahora tu y #{friend.full_name} son amigos"
+    else
+      flash[:notice] = "Hubo un error con tu solicitud"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
